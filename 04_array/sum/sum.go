617adeb46d74114fad39890bd459c59f55f895b3
Wu Chungyi <chungyiwu@qnap.com>
2023-08-07 18:48:39 +0800

    Refector Sum using for range.wq

--- 04_array/sum/sum.go
+++ 04_array/sum/sum.go
@@ -2,8 +2,9 @@ package main
 
 // Calculate summary of 5 numbers
 func Sum(numbers [5]int) (sum int) {
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, v := range numbers {
+		sum += v
 	}
+
 	return sum
 }
