70a7ea936b60767d6b8af7cffedee3e0acb0197d
Wu Chungyi <chungyiwu@qnap.com>
2023-08-08 14:28:27 +0800

    Refactor TestAera for more readable and easily debug

--- 05_struct_method_interface/shapes/shapes_test.go
+++ 05_struct_method_interface/shapes/shapes_test.go
@@ -20,20 +20,24 @@ func TestPerimeter(t *testing.T) {
 
 func TestAera(t *testing.T) {
 	aeraTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.16},
-		{Triangle{12, 6}, 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.16},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
 	for _, tt := range aeraTests {
-		got := tt.shape.Aera()
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Aera()
 
-		tolerance := 0.01
-		if diff := math.Abs(got - tt.want); diff > tolerance {
-			t.Errorf("got %.2f want %.2f", got, tt.want)
-		}
+			tolerance := 0.01
+			if diff := math.Abs(got - tt.want); diff > tolerance {
+				t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.want)
+			}
+		})
+
 	}
 }
