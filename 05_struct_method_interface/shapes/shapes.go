cd6c88b474a872fec5639c570d13cd472a9f5d53
Wu Chungyi <chungyiwu@qnap.com>
2023-08-08 12:11:22 +0800

    Refactor for test any aera of shapes.

--- 05_struct_method_interface/shapes/shapes.go
+++ 05_struct_method_interface/shapes/shapes.go
@@ -1,14 +1,34 @@
 package shapes
 
+import (
+	"math"
+)
+
+type Shape interface {
+	Aera() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(x, y float64) (peri float64) {
-	return 2 * (x + y)
+func (r Rectangle) Aera() (aera float64) {
+	return r.Width * r.Height
 }
 
-func Aera(x, y float64) (aera float64) {
-	return (x * y)
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Aera() (aera float64) {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rect Rectangle) (peri float64) {
+	return 2 * (rect.Height + rect.Height)
+}
+
+func Aera(rect Rectangle) (aera float64) {
+	return (rect.Height * rect.Width)
 }
--- 05_struct_method_interface/shapes/shapes_test.go
+++ 05_struct_method_interface/shapes/shapes_test.go
@@ -1,9 +1,16 @@
 package shapes
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{
+		Width:  10.0,
+		Height: 10.0,
+	}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -12,11 +19,26 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestAera(t *testing.T) {
-	got := Aera(12.0, 6.0)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	checkAera := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Aera()
+
+		tolerance := 0.001
+		if diff := math.Abs(got - want); diff > tolerance {
+			t.Errorf("got %f want %f diff %f", got, want, diff)
+		}
 	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{
+			Width:  12.0,
+			Height: 6.0,
+		}
+		checkAera(t, rectangle, 72.0)
 
+	})
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{Radius: 10}
+		checkAera(t, circle, 314.16)
+	})
 }
