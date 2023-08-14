bcc330b76241699387aeb90350ad97037d852119
Wu Chungyi <chungyiwu@qnap.com>
2023-08-14 11:20:51 +0800

    Add ch11

--- 11_select/racer/racer.go
+++ 11_select/racer/racer.go
@@ -6,6 +6,21 @@ import (
 	"time"
 )
 
+var tenSecondTimeout = 10 * time.Second
+
+func ConfigurableRacer(url1, url2 string,
+	timeout time.Duration) (winner string, err error) {
+	select {
+	case <-ping(url1):
+		return url1, nil
+	case <-ping(url2):
+		return url2, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("time out waiting for %s and %s", url1, url2)
+	}
+
+}
+
 func WebSiteRacer(url1, url2 string) (winner string, err error) {
 	// aDuration := measureResponseTime(url1)
 	// bDuration := measureResponseTime(url2)
@@ -15,14 +30,15 @@ func WebSiteRacer(url1, url2 string) (winner string, err error) {
 	// }
 	// return url2
 
-	select {
-	case <-ping(url1):
-		return url1, nil
-	case <-ping(url2):
-		return url2, nil
-	case <-time.After(10 * time.Second):
-		return "", fmt.Errorf("time out waiting for %s and %s", url1, url2)
-	}
+	// select {
+	// case <-ping(url1):
+	// 	return url1, nil
+	// case <-ping(url2):
+	// 	return url2, nil
+	// case <-time.After(10 * time.Second):
+	// 	return "", fmt.Errorf("time out waiting for %s and %s", url1, url2)
+	// }
+	return ConfigurableRacer(url1, url2, tenSecondTimeout)
 }
 
 func measureResponseTime(url string) time.Duration {
