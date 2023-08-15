07b2a5b47219e2a33af6d8a11619263e0ad84936
Wu Chungyi <chungyiwu@qnap.com>
2023-08-15 10:56:46 +0800

    Add ch13

--- 13_sync/counter/counter_test.go
+++ 13_sync/counter/counter_test.go
@@ -1,16 +1,24 @@
 package counter
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		coutner := Counter{}
-		coutner.Inc()
-		coutner.Inc()
-		coutner.Inc()
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
 
-		if coutner.Value() != 3 {
-			t.Errorf("got %d, want %d", coutner.Value(), 3)
-		}
+		assertCount(t, counter, 3)
+
 	})
+}
+
+func assertCount(t *testing.T, got Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %d, want %d", got.Value(), want)
+	}
 }
