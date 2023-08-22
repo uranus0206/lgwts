06e8e72e3ca5be6dc5d16e367dd8ee4c65d6bb19
Wu Chungyi <chungyiwu@qnap.com>
2023-08-22 15:02:33 +0800

    Refactor for cancel of ch14

--- 14_context/store/store_test.go
+++ 14_context/store/store_test.go
@@ -1,9 +1,13 @@
 package store
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"go.uber.org/goleak"
 )
 
 type StubStore struct {
@@ -14,16 +18,76 @@ func (s *StubStore) Fetch() string {
 	return s.response
 }
 
+type SpyStore struct {
+	response  string
+	cancelled bool
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
+
 func TestStore(t *testing.T) {
-	data := "hello, world"
-	svr := Server(&StubStore{data})
+	// data := "hello, world"
+	// svr := Server(&StubStore{data})
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
+	// request := httptest.NewRequest(http.MethodGet, "/", nil)
+	// response := httptest.NewRecorder()
 
-	svr.ServeHTTP(response, request)
+	// svr.ServeHTTP(response, request)
 
-	if response.Body.String() != data {
-		t.Errorf("got %s, want %s", response.Body.String(), data)
-	}
+	// if response.Body.String() != data {
+	// 	t.Errorf("got %s, want %s", response.Body.String(), data)
+	// }
+
+	t.Run("tells store to cancel work if request is cancelled",
+		func(t *testing.T) {
+
+			data := "hello, world"
+			store := &SpyStore{response: data}
+			svr := Server(store)
+
+			request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+			cancellingCtx, cancel := context.WithCancel(request.Context())
+			time.AfterFunc(5*time.Millisecond, cancel)
+
+			request = request.WithContext(cancellingCtx)
+
+			response := httptest.NewRecorder()
+
+			svr.ServeHTTP(response, request)
+
+			if !store.cancelled {
+				t.Errorf("store was not told to cancel")
+			}
+		})
+
+	t.Run("return data from store", func(t *testing.T) {
+		defer goleak.VerifyNone(t)
+		data := "hello, world"
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if response.Body.String() != data {
+			t.Errorf("got %s, want %s", response.Body.String(), data)
+		}
+
+		if store.cancelled {
+			t.Errorf("store should not cancel")
+		}
+	})
+}
+
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module lgwts
 
 go 1.20
+
+require go.uber.org/goleak v1.2.1
